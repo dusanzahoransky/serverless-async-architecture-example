afe03183dee8cc1debc6941176d5c9a1deb8aee9
dusan <dusan@rezdy.com>
2019-05-28 11:31:17 +1000

    serverless template file

--- serverless.js
+++ serverless.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const template = require('reusable-serverless-template');
+const template = require('serverless-fragments');
 
 let serverlessYaml = template.load(path.join(__dirname, 'serverless/serverless.core.yml'));
 
